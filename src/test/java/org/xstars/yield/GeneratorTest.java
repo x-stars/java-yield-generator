c4eef1627aeae2fd8f49cb298458f807ae72d6e6
周建华 <zjh96@live.cn>
2020-05-16 22:11:56 +0800

    Update tests and maven plugins.

--- src/test/java/org/xstars/yield/GeneratorTest.java
+++ src/test/java/org/xstars/yield/GeneratorTest.java
@@ -1,18 +1,51 @@
 package org.xstars.yield;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Java 生成器模式测试。
  */
 public class GeneratorTest {
     /**
+     * 初始化生成器模式测试的新实例。
+     */
+    public GeneratorTest() {
+    }
+
+    /**
      * 测试主入口点。
      * 
      * @param args 程序启动参数。
      */
     public static void main(String[] args) {
-        try (Generator<Integer> range10 = GeneratorTest.generateRange(10)) {
+        GeneratorTest test = new GeneratorTest();
+        test.testGenerate();
+    }
+
+    /**
+     * 生成一定范围的整数序列。
+     * 
+     * @param max 整数序列的最大值。
+     * @return 从 0 到 {@code max} - 1 的整数序列。
+     */
+    private Generator<Integer> generateRange(int max) {
+        return new Generator<Integer>() {
+            @Override
+            public void generate() {
+                for (int i = 0; i < max; i++) {
+                    yield(i);
+                }
+            }
+        };
+    }
+
+    /**
+     * 测试生成器的生成过程。
+     */
+    @Test
+    public void testGenerate() {
+        try (Generator<Integer> range10 = this.generateRange(10)) {
             int compare = 0;
             for (int test : range10) {
                 if (test >= 5) {
@@ -23,22 +56,5 @@ public class GeneratorTest {
                 compare++;
             }
         }
-    }
-
-    /**
-     * 生成一定范围的整数序列。
-     * 
-     * @param max 整数序列的最大值。
-     * @return 从 0 到 {@code max} - 1 的整数序列。
-     */
-    private static Generator<Integer> generateRange(int max) {
-        return new Generator<Integer>() {
-            @Override
-            public void generate() {
-                for (int i = 0; i < max; i++) {
-                    yield(i);
-                }
-            }
-        };
     }
 }
